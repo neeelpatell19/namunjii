78a95db92b19f4e8757e725ad11b64935fc05374
webtesting22 <148150265+webtesting22@users.noreply.github.com>
2025-07-23 10:43:46 +0530

    Navigation Btns

--- src/Components/HomeComponents/TrendingDesigns/TrendingDesigns.jsx
+++ src/Components/HomeComponents/TrendingDesigns/TrendingDesigns.jsx
@@ -1,23 +1,54 @@
-import React from "react";
+import React, { useRef } from "react";
 import "./TrendingDesigns.css";
 import FashionClothesData from "../../DummyData/DummyData";
 import { Row, Col } from "antd";
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { FreeMode, Pagination, Autoplay } from 'swiper/modules';
+import { FreeMode, Pagination, Autoplay, Navigation } from 'swiper/modules';
 import 'swiper/css';
 import 'swiper/css/free-mode';
 import 'swiper/css/pagination';
+import 'swiper/css/navigation';
 import { MdArrowOutward } from "react-icons/md";
 import { Link } from "react-router-dom";
 
 const TrendingDesigns = () => {
+    const swiperRef = useRef(null);
+
+    const handlePrevClick = () => {
+        if (swiperRef.current && swiperRef.current.swiper) {
+            swiperRef.current.swiper.slidePrev();
+        }
+    };
+
+    const handleNextClick = () => {
+        if (swiperRef.current && swiperRef.current.swiper) {
+            swiperRef.current.swiper.slideNext();
+        }
+    };
+
     return (
         <div className="MainContainer TrendingDesigns marginBottom50">
             <div className="PaddingTop">
                 <div className="Container">
-                    <div className="marginBottom50">
-                        <h2>Trending Designs</h2>
-                        <p>Explore curated collections from our top designers.</p>
+                    <div className="FlexContainerRepair">
+                        <div className="marginBottom50">
+                            <h2>Trending Designs</h2>
+                            <p>Explore curated collections from our top designers.</p>
+                        </div>
+                        <div className="TrendingDesignsSlideNavigationContainer OnlyDesktopBtn">
+                            <button
+                                className="swiper-nav-btn prev-btn"
+                                onClick={handlePrevClick}
+                            >
+                                <img src="https://s3.ap-south-1.amazonaws.com/prepseed/prod/ldoc/media/ArrowLeft.png" alt="" />
+                            </button>
+                            <button
+                                className="swiper-nav-btn next-btn"
+                                onClick={handleNextClick}
+                            >
+                                <img src="https://s3.ap-south-1.amazonaws.com/prepseed/prod/ldoc/media/ArrowRight.png" alt="" />
+                            </button>
+                        </div>
                     </div>
                     <div>
                         {/* <Row gutter={[16, 16]}>
@@ -44,6 +75,7 @@ const TrendingDesigns = () => {
                             }
                         </Row> */}
                         <Swiper
+                            ref={swiperRef}
                             slidesPerView={3}
                             spaceBetween={30}
                             speed={800}
@@ -68,7 +100,7 @@ const TrendingDesigns = () => {
                             // pagination={{
                             //     clickable: true,
                             // }}
-                            modules={[FreeMode, Pagination, Autoplay]}
+                            modules={[FreeMode, Pagination, Autoplay, Navigation]}
                             className="mySwiper"
 
                         >
@@ -102,6 +134,22 @@ const TrendingDesigns = () => {
                                 ))
                             }
                         </Swiper>
+                        <div className="TrendingDesignsSlideNavigationContainer marginTop50 OnlyMobileBtn">
+                            <div>
+                            <button
+                                className="swiper-nav-btn prev-btn"
+                                onClick={handlePrevClick}
+                            >
+                                <img src="https://s3.ap-south-1.amazonaws.com/prepseed/prod/ldoc/media/ArrowLeft.png" alt="" />
+                            </button>
+                            <button
+                                className="swiper-nav-btn next-btn"
+                                onClick={handleNextClick}
+                            >
+                                <img src="https://s3.ap-south-1.amazonaws.com/prepseed/prod/ldoc/media/ArrowRight.png" alt="" />
+                            </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div >
